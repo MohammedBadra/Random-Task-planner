c7c98cbb3d06a04ef1448b164738422e8902abbc
Mohammed Badra <mohbadra21@gmail.com>
2021-01-31 17:19:19 -0800

    Automate CSV and fix timezone
    New features:
    1. Automatic monthly CSV file generation from month_template.csv
    2. Automatic shift to PST
    3. Total time counter

--- task_picker.py
+++ task_picker.py
@@ -1,39 +1,52 @@
-#Choosing and initializing the daily csv5
-from datetime import datetime
+#Choosing and initializing the daily csv
+from datetime import datetime, timezone, date
 import sys
 import pandas as pd
 import random
 import time
+import pytz
 
-#This part is just to offset cloud nine's (UTC) 8 hour difference
-from datetime import datetime
-from datetime import date
+utc_now = pytz.utc.localize(datetime.utcnow())
+pst_now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
 
-today = datetime.now().strftime("%d/%m/%Y/%H")
-splitted_today = today.split("/")
+raw_today = pst_now.strftime("%d/%m/%Y/%H")
+today = raw_today.split("/")
+###########################################################################################################################################
 
-csvname = ""
-if int(splitted_today[-1]) in range(0, 8):
-    if splitted_today[1] == "12":
-        csvname = "0" + str(int(splitted_today[0]) - 1) + "Dec.csv"
-else:
-    if splitted_today[1] == "12":
-        csvname = splitted_today[0] + "Dec.csv"
+#handling the monthyl database csv file
+months_dict = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
 
-print(csvname)
+year = str(int(today[2]) - 2000)
+month = months_dict[int(today[1])]
+
+csvname = month + year + ".csv"
+
+try:
+    tasks = pd.read_csv(csvname)
+except FileNotFoundError:
+    temp = pd.read_csv("month_template.csv")
+    temp.to_csv(csvname)
+    print("The file didn't exist. We created it for you! Please fill it and rerun!")
+    sys.exit()
+
 tasks = pd.read_csv(csvname)
+#tasks
+###########################################################################################################################################
 
 ###########################################################################################################################################
 #Initializing the planner
 
-available_tasks = [tasks.Task[index] for index in tasks.index if tasks.Done[index] == False]
+#retreiving today's date to parse for it in the csv. format: DD/MM/YYYY
+today_check = raw_today[:10]
+
+available_tasks = [tasks.Task[index] for index in tasks.index if (tasks.Done[index] == False) and (tasks.Date[index] == today_check)]
 availables_sum = 0
 final_break = 0
 running_time_total = 0
 task_count = 0
 
 for index in tasks.index:
-    if tasks.Done[index] == False:
+    if (tasks.Done[index] == False) and (tasks.Date[index] == today_check):
         availables_sum += (tasks.alotted_pomodoros[index] - tasks.finished_pomodoros[index])
 print("Overall, you can do", availables_sum, "pomodoros!\n")
 
@@ -41,6 +54,8 @@ pomodoro_slots = int(input("How many pomodoros do you want to do now?\n"))
 if pomodoro_slots > availables_sum:
     print("Please rerun after adding more tasks!")
     sys.exit()
+########################################################################################################################################### 
+
 ###########################################################################################################################################
 for pomodoro in range(pomodoro_slots):
     current_task = random.choice(available_tasks)
@@ -63,6 +78,7 @@ for pomodoro in range(pomodoro_slots):
             wait = input("Take a 5 minutes break and write anything when you're back!\n")
     else:
         final_break = (running_time_total / 25) * 5
+        print("Your total runtime:", running_time_total)
         print("Yay! You're done :) \nTake a ", round(final_break), "minutes break at least. You earned it!\n")
 
     for taskie_index in range(len(tasks.Task)):
@@ -71,7 +87,7 @@ for pomodoro in range(pomodoro_slots):
             if tasks.finished_pomodoros[taskie_index] == tasks.alotted_pomodoros[taskie_index]:
                 tasks.Done[taskie_index] = "True"
 
-    available_tasks = [tasks.Task[index] for index in tasks.index if tasks.Done[index] == False]
+    available_tasks = [tasks.Task[index] for index in tasks.index if (tasks.Done[index] == False) and (tasks.Date[index] == today_check)]
     #print(tasks)
     print("Tasks left:", available_tasks, "\n")
     task_count += 1
